4e1e7b2a5c321da2ffb04b88855c77edaf259ded
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-12-29 22:38:47 -0500

    notes controller complete

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,9 @@
 import express from "express";
-
+import notesRoutes from "./routes/notesRoutes.js";
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-  res.send("5 notes");
-});
-app.post("/api/notes", (req, res) => {
-  res.status(201).json({ message: "note created successfully" });
-});
-app.put("/api/notes/:id", (req, res) => {
-  res.status(200).json({ message: "note updated successfully" });
-});
-app.delete("/api/notes/:id", (req, res) => {
-  res.status(200).json({ message: "note deleted successfully" });
-});
+app.use("/api/notes", notesRoutes);
+
 app.listen(5000, () => {
   console.log("server started");
 });
