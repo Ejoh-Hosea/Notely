f5a8e5545bd6547b845e542a76696cece09614ce
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-12-31 00:53:33 -0500

    fetch data from database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+dotenv.config();
 
 import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
-dotenv.config();
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
 app.use(ratelimiter);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUi from "../components/RateLimitedUi";
-
+import axios from "axios";
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.log("error fetching");
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-screen">
       <Navbar />
