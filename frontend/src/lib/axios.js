3b01617352e026e93da6b249945631a707b1b565
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-01 12:18:52 -0500

    Prepared for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+//There is no local host in production
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
 });
 export default api;
